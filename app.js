9ceb0e5446a9680037da24e2e3e41b327c68eeb3
shubhanjali-creates <shubhijee2024@gmail.com>
2026-06-26 17:21:16 +0530

    updated aap.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require("path");
 const methodOverride =require("method-override");
 const ejsMate = require("ejs-mate");
 
-const MONGO_URL = 'mongodb://127.0.0.1:27017/wanderlust';
+const MONGO_URL = 'mongodb://127.0.0.1:27017/hackathon-app';
 main()
 .then(()=>{
     console.log("connected to DB");
